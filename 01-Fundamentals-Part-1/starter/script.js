0ec06e325f1803e51444b4c5ce29e1ea025cb9af
Fikayo <demixwillex@gmail.com>
2024-02-20 15:23:08 +0100

    First Commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,30 @@
+// let js = 'amazing';
+// console.log(40 + 8 +23 - 10);
+
+// let firstName = "Matilda";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let jonas_matilda = "JM";
+// let $function = 27;
+
+// let person = "jonas";
+// let PI = 3.142;
+
+// let myFirstJob = 'coder';
+// let myCurrentJob = 'Teacher'
+
+// let job1 = 'programmer';
+// let jo2 = 'teacher';
+
+// console.log(myFirstJob);
+
+let country = 'Nigeria';
+let continent = 'Africa';
+let population = 20000000000;
+
+console.log(country)
+console.log(continent)
+console.log(population)
