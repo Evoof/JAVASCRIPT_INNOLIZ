e909b0ddb836559c930c8cfb52c659da4a50999f
Fikayo <demixwillex@gmail.com>
2024-03-05 07:59:03 +0100

    Javascript Fundamental

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -63,7 +63,9 @@ console.log(age3);
 const yearsUntilRetirement = (birthYear, firstName) => {
     const age = 2037 - birthYear;
     const retirement = 65 - age;
-    return retirement;
+    // return retirement;
+    return`${firstName} retires in ${retirement} years`
 }
 
-console.log(yearsUntilRetirement(1991));
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
