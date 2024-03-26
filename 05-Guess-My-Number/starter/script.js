68ecd85148e516c8cf0189eca4ad42561567c1ed
Fikayo <demixwillex@gmail.com>
2024-03-26 09:15:36 +0100

    Guess_My_Number_game

---
+++ 05-Guess-My-Number/starter/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// console.log(
+//   (document.querySelector('.message').textContent = 'Correct Number! 🎉')
+// );
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+const number = Math.trunc(Math.random() * 20 + 1);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '❌ No number found!';
+  }
+});
